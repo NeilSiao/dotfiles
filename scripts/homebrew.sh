e8b28275431da407f6bd5115a32bbba317512eea
Ray Chang <raychang@pixnet.tw>
2019-02-27 12:03:14 +0800

    add homebrew.sh

---
+++ scripts/homebrew.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+FORMULA="autoconf bat diff-so-fancy exa fd fzf git go htop hugo kubernetes-helm kubetail lua mycli mysql node openssl pcre pcre2 perl php php@7.1 phpmd phpunit prettyping pstree psysh python python@2 readline rtmpdump ruby sift sqlite tig tmux vim wrk yarn zsh zsh-completions"
+
+brew install $FORMULA;
